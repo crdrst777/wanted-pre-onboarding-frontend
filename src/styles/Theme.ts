26cce9da0205f93bfd210b0c27ddf6660ebbdf38
HYEJINLEE <crdrst777@gmail.com>
2023-01-28 16:35:21 +0900

    feat: 초기세팅

---
+++ src/styles/Theme.ts
@@ -0,0 +1,17 @@
+const calcRem = (size: number) => `${size / 16}rem`;
+
+const fontSizes = {
+  xs: calcRem(12),
+  small: calcRem(14),
+  base: calcRem(16),
+  lg: calcRem(18),
+  xl: calcRem(20),
+  xxl: calcRem(24),
+  titleSize: calcRem(32),
+};
+
+const theme = {
+  fontSizes,
+};
+
+export default theme;
